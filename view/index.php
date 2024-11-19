41f6deb2f9d8dd09577379c7b24edcdbbe60e2cc
Jeison amell <jamells@unicartagena.edu.co>
2024-11-18 19:10:15 -0500

    hola mundo

---
+++ view/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hola XD</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container mt-5">
+        <h1 class="text-center">Que vivan los gatos y las tetas</h1>
+        <div class="row">
+            <?php  ?>
+                <div class="col-12">
+                    <p class="text-center">Hola BUAJAJJAJAJAJAJA.</p>
+                </div>
+            <?php?>
+        </div>
+    </div>
+</body>
+</html>
